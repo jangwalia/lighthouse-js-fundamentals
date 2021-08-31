896fbcc954dcfd3bac94d1271af66447e26c04dd
jang walia <jangiwalia83@gmail.com>
2021-08-31 10:25:25 -0700

    second commit for weather

--- conditional.js
+++ conditional.js
@@ -1,10 +1,6 @@
-const raining = true;
 const cold = false;
-if(raining){
-  console.log("don't forget your umbrella");
-
-}
 if(cold){
-  console.log("make sure you pick out the scarf");
+  console.log("Don't forget to wear jacket ...")
+}else{
+  console.log('enjoy the run...its beautiful weather outside');
 }
-console.log('Now we are ready to go outside');
