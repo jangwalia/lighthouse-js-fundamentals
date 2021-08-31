60e9f06bdfbcf6c858046456fd76e9f1e1b8e8e3
jang walia <jangiwalia83@gmail.com>
2021-08-31 10:38:18 -0700

    commit for voting criteria

--- conditional.js
+++ conditional.js
@@ -1,11 +1,5 @@
-const temprature = 20;
-if(temprature<5){
-  console.log("postpone the plan for running... its way too cold");
-}else if(temprature<12 && temprature>5){
-  console.log('we can go for run..but wear sarf and jacket');
-}else if(temprature>12 && temprature<30){
-  console.log('this is the best time to workout outside');
-}else{
-  console.log('this is too hot for outside run...dring plenty of fluids..');
+const isCitizen = true;
+const age = 20;
+if(isCitizen && age>18){
+  console.log('congratulation..you are eligible to vote');
 }
-console.log('This is your today\'s forcast to workout based on weather');
