d8e97eabd800b6d4347c720a043214775bf97441
jang walia <jangiwalia83@gmail.com>
2021-08-31 10:18:11 -0700

    checking weather to go out

---
+++ conditional.js
@@ -0,0 +1,10 @@
+const raining = true;
+const cold = false;
+if(raining){
+  console.log("don't forget your umbrella");
+
+}
+if(cold){
+  console.log("make sure you pick out the scarf");
+}
+console.log('Now we are ready to go outside');
