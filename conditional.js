fdb48311af3164679e068b33a45322cd36c8cba0
jang walia <jangiwalia83@gmail.com>
2021-08-31 10:34:49 -0700

    commit for weather forcast based on temrature

--- conditional.js
+++ conditional.js
@@ -1,6 +1,11 @@
-const cold = false;
-if(cold){
-  console.log("Don't forget to wear jacket ...")
+const temprature = 20;
+if(temprature<5){
+  console.log("postpone the plan for running... its way too cold");
+}else if(temprature<12 && temprature>5){
+  console.log('we can go for run..but wear sarf and jacket');
+}else if(temprature>12 && temprature<30){
+  console.log('this is the best time to workout outside');
 }else{
-  console.log('enjoy the run...its beautiful weather outside');
+  console.log('this is too hot for outside run...dring plenty of fluids..');
 }
+console.log('This is your today\'s forcast to workout based on weather');
