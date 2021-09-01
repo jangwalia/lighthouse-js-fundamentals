147573fbd4c613bcc9b06db5ea3e9bee37c0a0b4
jang walia <jangiwalia83@gmail.com>
2021-09-01 11:40:53 -0700

    second commit with changing key

--- chorus-while.js
+++ chorus-while.js
@@ -1,7 +1,12 @@
 const chorus = "lets dance";
 let rept = 0;
 while(rept<10){
+  if(rept===5){
+    console.log('change key');
+  }
+  
   console.log(chorus);
+  
   rept++;
 }
 console.log('until the sun comes up...')
