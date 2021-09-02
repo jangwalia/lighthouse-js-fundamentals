b7c886d1108aed4a6d544f8b7cab8f4f2d91155f
jang walia <jangiwalia83@gmail.com>
2021-09-02 15:27:56 -0700

    isEven function final commit

---
+++ even.js
@@ -0,0 +1,8 @@
+//Basic function expression and calling
+const isEven = function(num){
+  return num%2 === 0;
+}
+let Num1 = isEven(10);
+let Num2 = isEven(15);
+console.log(Num1);
+console.log(Num2);
