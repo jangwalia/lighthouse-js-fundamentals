ee4bf696fc6e9db14d64d634d4f9bd2b285b03e3
jang walia <jangiwalia83@gmail.com>
2021-09-03 06:20:36 -0700

    odd checker commit

---
+++ odd.js
@@ -0,0 +1,9 @@
+function isOdd(num){
+  if(num % 2 !== 0){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log("15 is Odd Number: " + isOdd(15));
+console.log("28 is Odd Number: " + isOdd(28));
