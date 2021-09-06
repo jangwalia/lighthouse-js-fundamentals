8ae24cbefbd7e8b299aebed54e0147b8c05e1706
jang walia <jangiwalia83@gmail.com>
2021-09-06 09:27:55 -0700

    commit to using while  loop through array

--- packing-list.js
+++ packing-list.js
@@ -1,5 +1,7 @@
 const packageList = ["bowls","plates","pots","eating utensils","glasses","cups","cooking utensils"];
 console.log("kitchen stuff to pack");
-for(var i = 0;i < packageList.length; i++){
+let i = 0;
+while( i < packageList.length){
   console.log(packageList[i]);
+  i++;
 }
