22d5b130ca160069984e4ec080da1db2cca747c1
jang walia <jangiwalia83@gmail.com>
2021-09-06 09:24:48 -0700

    first committo loop through loop

---
+++ packing-list.js
@@ -0,0 +1,5 @@
+const packageList = ["bowls","plates","pots","eating utensils","glasses","cups","cooking utensils"];
+console.log("kitchen stuff to pack");
+for(var i = 0;i < packageList.length; i++){
+  console.log(packageList[i]);
+}
